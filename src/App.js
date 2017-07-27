82730588f048a524650fb26c23884d0212f36b32
Petter Östergren <petter.ostergren@icloud.com>
2017-07-27 13:19:26 +0200

    refactor: Added call function to BooksAPI

--- src/App.js
+++ src/App.js
@@ -7,21 +7,33 @@ import MainBookWrapper from './bookComponents/MainBookWrapper'
 
 class BooksApp extends React.Component {
   state = {
-    
+    books: []
+
   }
+
+  componentDidMount(){
+    BooksAPI.getAll().then(books => {
+      console.log(books)
+      this.setState({ books })
+  })
+}
+
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
+
           <MainBookWrapper
+
+
 
           />
 
         )}/>
         <Route path="/search" render={() => (
           <SearchBooks
-
+            books={this.props.books}
           />
 
         )}/>
