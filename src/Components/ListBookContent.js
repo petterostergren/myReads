d55712c467957320a05cac98d36bcc41695ca65d
Petter Östergren <petter.ostergren@icloud.com>
2017-08-04 23:08:14 +0200

    refactor: Re-wrote bookshelf to be DRY

--- src/Components/ListBookContent.js
+++ src/Components/ListBookContent.js
@@ -1,33 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import BookShelf from './BookShelf'
 
-const ListBooksContent = ({books, bookStateUpdated}) => (
-  <div>
-      <BookShelf
-        key={"currentlyReading"}
-        books={books.filter(b => b.shelf === "currentlyReading")}
-        bookStateUpdated={bookStateUpdated}
-        shelfLabel={"currentlyReading"}
-        title={"Currently Reading"}
-      />
-      <BookShelf
-        key={"wantToRead"}
-        books={books.filter(b => b.shelf === "wantToRead")}
-        bookStateUpdated={bookStateUpdated}
-        shelfLabel={"wantToRead"}
-        title={"Want to Read"}
-      />
-      <BookShelf
-        key={"read"}
-        books={books.filter(b => b.shelf === "read")}
-        bookStateUpdated={bookStateUpdated}
-        shelfLabel={"read"}
-        title={"Read"}
-      />
-  </div>
 
-)
+class ListBooksContent extends Component {
+  bookShelfLabels = [
+      {title: "Currently Reading",
+       label: "currentlyReading"
+      },
+      {
+       title: "Want to Read",
+       label: "wantToRead"
+      },
+      {
+       title: "Read",
+       label: "read"
+      }
+  ]
+
+
+  render() {
+
+    return(
+      <div className="list-books-content">
+          {this.bookShelfLabels.map((value) => (
+            <BookShelf
+              key={value.label}
+              books={this.props.books.filter(b => b.shelf === value.label)}
+              bookStateUpdated={this.props.bookStateUpdated}
+              shelfLabel={value.label}
+              title={value.title}
+            />
+          ))}
+      </div>
+    )
+  }
+}
 
 
 ListBooksContent.propTypes = {
