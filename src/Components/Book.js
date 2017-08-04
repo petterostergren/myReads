728a3958df169404d45c3bb18f10c37f1d544581
Petter Östergren <petter.ostergren@icloud.com>
2017-08-04 23:07:24 +0200

    refactor: Book cover hovering to be an CSS effect instead of a state

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,70 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import BookShelfChanger from './BookShelfChanger'
 
 
 
-class Book extends Component {
-  state = {
-    isHovered: false,
-  }
-
-
-  /**
-  * @description Changes state of isHovered to enable overlaybox
-  */
-  onMouseEnter = () => {
-    this.setState({isHovered: true})
-
-
-  }
-
-  /**
-  * @description Changes state of isHovered to disabled overlaybox
-  */
-  onMouseLeave = () => {
-    this.setState({isHovered : false})
-
-  }
-
-
-  render() {
-    return(
+const Book = ({book, bookStateUpdated}) => (
         <div className="book">
           <div className="book-top">
-            <Link to={`/details/${this.props.book.id}`}>
+            <Link to={`/details/${book.id}`}>
               <div
                 className="book-cover"
-                onMouseEnter={() => {this.onMouseEnter()}}
-                onMouseLeave={() => {this.onMouseLeave()}}
                 style={{ width: 128, height: 193,
-                  backgroundImage: `url(${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : 'http://dvepublishing.com/images/cover_not_available.jpg'})` }}>
-
-                  {/*
-                    Creates a overlay ontop of the book cover to
-                    guide the user on where to find more information
-                  */}
-                  {this.state.isHovered ? (
-                    <div className="hover-box"></div>) :
-                  (
-                    <div />
-                  )}
-                  </div>
+                  backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : 'http://dvepublishing.com/images/cover_not_available.jpg'})` }}>
+              </div>
             </Link>
             <div className="book-shelf-changer">
             <BookShelfChanger
-              book = {this.props.book}
-              bookStateUpdated={this.props.bookStateUpdated}
+              book = {book}
+              bookStateUpdated={bookStateUpdated}
             />
             </div>
           </div>
-          <div className="book-title">{this.props.book.title}</div>
-          <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(", ") : "No Author Given" }</div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors ? book.authors.join(", ") : "No Author Given" }</div>
         </div>
     )
-  }
-}
+
 
 Book.propTypes = {
   book: PropTypes.object.isRequired,
